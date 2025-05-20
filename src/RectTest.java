abfe548f72a84d5d70718391f5dd0a042bda67d6
MSI\blank <zeal24360066@fsgcl.jp>
2025-05-20 12:30:28 +0900

    Initalize

--- src/RectTest.java
+++ src/RectTest.java
@@ -1,10 +1,12 @@
 
-public class start {
+public class RectTest extends MyFrame{
+	public void run() {
+		fillRect(30,50,200,100);
+	}
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("こんにちは");
-		MyFrame frame1=new RectTest();
+		
 	}
 
 }

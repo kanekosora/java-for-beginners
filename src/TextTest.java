1c19e7a0a8151786f86605d9606ce26f8efbdb67
MSI\blank <zeal24360066@fsgcl.jp>
2025-05-26 11:05:59 +0900

    "おはよう","こんにちは"

---
+++ src/TextTest.java
@@ -0,0 +1,13 @@
+
+public class TextTest extends MyFrame{
+	public void run() {
+		drawString("おはよう",100,100,15);
+		drawString("こんにちは",100,200,20);
+	}
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}

233f0b98d0f5fbc13b8e913d6ea1abc90f848e9e
MSI\blank <zeal24360066@fsgcl.jp>
2025-05-26 10:23:40 +0900

    色

--- src/RectTest.java
+++ src/RectTest.java
@@ -1,7 +1,24 @@
 
 public class RectTest extends MyFrame{
 	public void run() {
-		fillRect(30,50,200,100);
+		
+		setColor(255,255,0);
+		fillRect(30,40,50,100);
+		
+		setColor(200,0,255);
+		fillRect(130,40,50,100);
+		
+		setColor(130,220,255);
+		fillRect(230,40,50,100);
+		
+		setColor(0,0,0);
+		fillRect(330,40,50,100);
+		
+		setColor(150,150,150);
+		fillRect(30,150,50,100);
+		
+		setColor(255,255,255);
+		fillRect(35,45,40,40);
 	}
 
 	public static void main(String[] args) {

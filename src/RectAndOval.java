9e2d0c635a40272ec26292f49cb96fccca04eefe
MSI\blank <zeal24360066@fsgcl.jp>
2025-05-26 11:56:31 +0900

    for文RectLoop

--- src/RectAndOval.java
+++ src/RectAndOval.java
@@ -2,10 +2,10 @@
 public class RectAndOval extends MyFrame{
 	public void run() {
 		
-		fillRect(150,140,100,30);
-		fillRect(170,120,60,30);
-		fillOval(165,170,25,25);
-		fillOval(210,170,25,25);
+		fillRect(50,140,100,30);
+		fillRect(70,120,60,30);
+		fillOval(65,170,25,25);	
+		fillOval(110,170,25,25);
 	}
 
 	public static void main(String[] args) {
---
+++ src/RectLoop.java
@@ -0,0 +1,18 @@
+
+public class RectLoop extends MyFrame{
+	public void run() {
+		setColor(0,128,0);
+		int x = 30;
+		for(int i = 0; i < 10; i++) {
+			System.out.println(x);
+			fillRect(x,80,10,100);
+			x += 20;
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}

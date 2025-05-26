812ddaad8efd0ec9910f93a357614b1362fb56f2
MSI\blank <zeal24360066@fsgcl.jp>
2025-05-26 11:21:09 +0900

    車

---
+++ src/RectAndOval.java
@@ -0,0 +1,16 @@
+
+public class RectAndOval extends MyFrame{
+	public void run() {
+		
+		fillRect(150,140,100,30);
+		fillRect(170,120,60,30);
+		fillOval(165,170,25,25);
+		fillOval(210,170,25,25);
+	}
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
--- src/start.java
+++ src/start.java
@@ -3,7 +3,7 @@ public class start {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("こんにちは");
-		MyFrame frame1=new TextTest();
+		MyFrame frame1=new RectAndOval();
 	}
 
 }
